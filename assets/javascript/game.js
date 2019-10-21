52aad8490df1b0f50bf7c7b8218473c6c6d00833
Abiel Hailemariam <abielh06@gmail.com>
2019-10-20 20:23:30 -0500

    made changes to the logic code and css.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,97 @@
 
-    
 
+// Global variables
 
-    $(document).ready(function(){
-    var randNumber = 0
-    var diamond = 0
-    var emerald = 0
-    var ruby = 0
-    var topaz = 0
-   var randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-    $('#randNum').html(randNumber);
-
-    var diamond = Math.floor(Math.random() * 12) + 1;
-    var emerald = Math.floor(Math.random() * 12) + 1;
-    var ruby = Math.floor(Math.random() * 12) + 1;
-    var topaz = Math.floor(Math.random() * 12) + 1;
-
-   
-    var losses = 0;  
-
-    var totScore = 0;
-    $('#totScore').text(totScore);
-
-    function win() {
-        wins = wins + 1;
-        $('#wins').text(wins);
+var jewel = {
+    diamond:{
+        value:0
+    },
+    emerald:{
+        value: 0
+    },
+    ruby:{
+        value:0
+    }, 
+    topaz:{
+        value:0
     }
+}
+var currentScore = 0;
+var targetScore = 0;
 
-    function lose() {
-        losses = losses + 1;
-        $('#losses').text(losses);
-        
+var wins = 0;
+var losses = 0;
+
+
+//functions
+
+var getRandom = function(min, max){
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+
+}
+
+var startGame = function () {
+    // reset current score
+    currentScore = 0;
+    // set a new target score between 19 - 120
+    targetScore = getRandom(19,120);
+    // set different values for each of the crystal b/w 1-12
+    jewel.diamond.value     = getRandom(1,12);
+    jewel.emerald.value     = getRandom(1,12);
+    jewel.ruby.value        = getRandom(1,12);
+    jewel.topaz.value       = getRandom(1,12);
+
+    $("#totScore").html(currentScore);
+    $("#targetScore").html(targetScore);
+
+    // console.log("--------------")
+    // console.log("target score: "+ targetScore)
+    // console.log("jewel.diamond.value + "|"jewel.emerald.value +"|"jewel.ruby.value +"|"jewel.topaz.value")
+    // console.log("--------------")
+    // change the HTML to reflect  changes
+
+}
+
+var addValues = function(jewel){
+
+
+    currentScore = currentScore + jewel.value;
+
+    $("#totScore").html(currentScore);
+
+    checkWin();
+
+    console.log("your score; "+ currentScore);
+}
+var checkWin = function(){
+    if(currentScore > targetScore){
+        console.log("you lost");
+        losses ++;
+        $("#losses").html(losses);
+    startGame();
     }
-    // working
-    // console.log(randNumber); 
-
-  
-    function reset() {
-        totScore = 0;
-        $('#totScore').text(totScore);
+    else if (currentScore == targetScore){
+    console.log("you won")
+    wins ++;
+    $("#wins").html(wins);
+    startGame();
     }
+}
 
-    
+startGame()
+
+$("#diamond").click(function() {
+    addValues(jewel.diamond);
+
 });
+$("#emerald").click(function() {
+    addValues(jewel.emerald);
+});
+$("#ruby").click(function() {
+    addValues(jewel.ruby);
+});
+$("#topaz").click(function() {
+    addValues(jewel.topaz);
+});
+    
+            
